fe4d5079580c672e6354b0397eac0ca1d7d63012
David <davidschonenberger@gmail.com>
2025-04-10 15:17:43 +0200

    añadido creacion de sockets al server

--- server.c
+++ server.c
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
  * File:   main.c
@@ -23,7 +18,7 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
+#include <arpa/inet.h>
 
 #define MAX_CLIENTS 100
 #define BUFFER 1024
@@ -31,18 +26,54 @@
 #define SEM "/chat_sem"
 
 
-typedef struct{
-    int in_add;
-    int out_add;
-    char username[BUFFER];
-    int active;
-    char in_pipe[BUFFER];
-    char out_pipe[BUFFER];
-}client_t;
-
-client_t* clients;  
+int clients_fd[MAX_CLIENTS];  
 sem_t* sem;
 int running = 1;
+
+
+void configure_sigusr2_handler() {
+    struct sigaction hand;
+    sigset_t mask;
+    sigemptyset(&mask);
+    hand.sa_mask = mask;
+    hand.sa_flags = 0;
+    hand.sa_handler = sigusr2_handler;
+    sigaction(SIGINT, &hand, NULL);
+}
+
+int create_listening_socket(int port) {
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket creation failed");
+        return -1;
+    }
+
+    // Set up server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    // Bind socket
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind failed");
+        close(sockfd);
+        return -1;
+    }
+
+    // Start listening
+    if (listen(sockfd, 5) == -1) {
+        perror("listen failed");
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
+}
 
 void sigusr2_handler(int sig){
     running=0; // RECIBIR SIGURS2 === DEJAR DE ACEPTAR CLIENTES
@@ -96,13 +127,25 @@ void handle_client(int index){
 }
 
 int main(int argc, char** argv) {
-    signal(SIGUSR2,sigusr2_handler);
-    int shm_fd=shm_open(SHM, O_CREAT | O_RDWR, 0666);
-    ftruncate (shm_fd,MAX_CLIENTS*sizeof(client_t));
-    clients=mmap(NULL,MAX_CLIENTS*sizeof(client_t),PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd,0);
-    sem = sem_open(SEM,O_CREAT,0666,1);
-    printf("Server active, waiting for clients...\n");
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <port-in> <port-out>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
     
+    configure_sigusr2_handler();
+    int socket_in = create_listening_socket(atoi(argv[1]));
+    int socket_out = create_listening_socket(atoi(argv[2]));
+
+    struct sockaddr_in sa_r;
+    socklen_t const addr_len = sizeof(sa_r);
+    while(1) {
+        int fd_in = accept(socket_in, (struct sockaddr*) &sa_r, &addr_len);
+        int fd_out = accept(socket_out, (struct sockaddr*) &sa_r, &addr_len);
+    }
+
+    
+
     
     while (running){
         char uname[BUFFER],in_pipe[BUFFER],out_pipe[BUFFER];
